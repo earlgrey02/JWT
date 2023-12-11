1c01a3f4e7bbe8af2835b706b79faba0be113a39
earlgrey02 <san06036@naver.com>
2023-12-11 21:56:50 +0900

    Chore: 테스트 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks {
