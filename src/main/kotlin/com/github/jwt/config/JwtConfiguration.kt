bd1c811ece3ece3b50d63ac9c48afb06661b9736
earlgrey02 <san06036@naver.com>
2024-05-09 19:22:55 +0900

    Feat: JwtConfiguration이 다른 JwtProvider가 Bean으로 등록되지 않은 경우에만 작동하도록 변경

--- src/main/kotlin/com/github/jwt/config/JwtConfiguration.kt
+++ src/main/kotlin/com/github/jwt/config/JwtConfiguration.kt
@@ -1,36 +1,39 @@
 package com.github.jwt.config
 
+import com.github.jwt.core.DefaultJwtProvider
 import com.github.jwt.core.JwtProvider
-import io.jsonwebtoken.io.Decoders
-import io.jsonwebtoken.security.Keys
+import com.github.jwt.util.minutesToMillis
+import com.github.jwt.util.toSecretKey
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
-import java.util.concurrent.TimeUnit
 
 /**
- * [AutoConfiguration] class that registers [JwtProvider] as a [Bean].
+ * [AutoConfiguration] class that registers [DefaultJwtProvider] as a Bean.
+ * Class that is disabled when another [JwtProvider] exists as a Bean.
  */
 @AutoConfiguration
+@ConditionalOnMissingBean(JwtProvider::class)
 class JwtConfiguration {
     /**
-     * [JwtProvider] with properties injected.
+     * [DefaultJwtProvider] with properties injected.
      *
-     * @param secretKey Secret key
-     * @param accessTokenExpire Access token validity time(m)
-     * @param refreshTokenExpire Access token validity time(m)
+     * @param secret Secret string
+     * @param accessTokenExpire Access token validity time(minute)
+     * @param refreshTokenExpire Access token validity time(minute)
      */
     @Bean
     fun jwtProvider(
-        @Value("\${jwt.secretKey}")
-        secretKey: String,
+        @Value("\${jwt.secret}")
+        secret: String,
         @Value("\${jwt.accessTokenExpire}")
         accessTokenExpire: Long,
         @Value("\${jwt.refreshTokenExpire}")
         refreshTokenExpire: Long
-    ): JwtProvider = JwtProvider(
-        secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)),
-        accessTokenExpire = TimeUnit.MINUTES.toMillis(accessTokenExpire),
-        refreshTokenExpire = TimeUnit.MINUTES.toMillis(refreshTokenExpire)
+    ): DefaultJwtProvider = DefaultJwtProvider(
+        secretKey = secret.toSecretKey(),
+        accessTokenExpire = minutesToMillis(accessTokenExpire),
+        refreshTokenExpire = minutesToMillis(refreshTokenExpire)
     )
 }
